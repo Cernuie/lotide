507c5a45a43a5ff5ce9ded6f41d272ebd9db0a79
Alexander Nguyen <cernvii@gmail.com>
2021-01-04 19:31:08 +0000

    linted

--- head.js
+++ head.js
@@ -7,12 +7,12 @@ const assertEqual = function(actual, expected) {
 };
 
 const head = function(array) {
-  if (array.length < 1){
-    return undefined
+  if (array.length < 1) {
+    return undefined;
   } else {
-    return(array[0]);
+    return (array[0]);
   }
-}
+};
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
