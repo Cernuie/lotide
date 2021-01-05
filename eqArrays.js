d052871f9f616655f5f341bcf77b10cb2b6da0bd
Alexander Nguyen <cernvii@gmail.com>
2021-01-05 20:11:20 +0000

    linted

--- eqArrays.js
+++ eqArrays.js
@@ -7,18 +7,18 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(first, second) {
-  //first check length 
+  //first check length
   if (first.length === second.length) {
-    // then check each element equal to each other 
-    for (i = 0; i < first.length; i++) {
+    // then check each element equal to each other
+    for (let i = 0; i < first.length; i++) {
       if (first[i] !== second[i]) {
-        console.log(i)
+        console.log(i);
         return false;
       }
     }
   }
   return true;
-}
+};
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
