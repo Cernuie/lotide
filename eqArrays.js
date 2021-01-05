6314e79e9f02a820c2eec4a796e0dd6ed657617e
Alexander Nguyen <cernvii@gmail.com>
2021-01-05 20:07:08 +0000

    equal array checker

---
+++ eqArrays.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(first, second) {
+  //first check length 
+  if (first.length === second.length) {
+    // then check each element equal to each other 
+    for (i = 0; i < first.length; i++) {
+      if (first[i] !== second[i]) {
+        console.log(i)
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
