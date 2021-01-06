00675482780d95a4758321bc7515dbe7ffa1ad0b
Alexander Nguyen <cernvii@gmail.com>
2021-01-06 20:30:09 +0000

    started typing function

--- eqObjects.js
+++ eqObjects.js
@@ -6,3 +6,7 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+};
